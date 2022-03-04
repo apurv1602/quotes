16e1c64e613a9a73317754dbc5d211d7e8442dee
apurv_1602 <apurvjagtap16@gmail.com>
2022-03-04 12:18:49 +0530

    1.1 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quotes/quote.dart';
 import 'quote.dart';
-import 'quote.dart';
+
 
 void main() => runApp(MaterialApp(
     home: Quotea(),
@@ -21,17 +21,47 @@ class _QuoteaState extends State<Quotea> {
     Quote(author:'apurv', text:'fuck you'),
   ];
 
+  Widget quoteTemplet(quote){
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.grey[600],
+              ),
+            ),
+          const SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[800],
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Awesome qoute'),
+        title: const Text('Awesome qoute'),
         centerTitle: true,
            backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quote.map((quote) =>  Text('${quote.text} - ${quote.author}')).toList(),
+        children: quote.map((quote) => quoteTemplet(quote) ).toList(),
       ),
     );
   }
